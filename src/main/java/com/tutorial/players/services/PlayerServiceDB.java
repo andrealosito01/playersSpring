1b561c264fee44e43be62837f69da464012b59db
andrealosito01 <andrealosito01@gmail.com>
2023-05-14 16:09:38 +0200

    Added Spring Cache

--- src/main/java/com/tutorial/players/services/PlayerServiceDB.java
+++ src/main/java/com/tutorial/players/services/PlayerServiceDB.java
@@ -3,6 +3,9 @@ package com.tutorial.players.services;
 import com.tutorial.players.models.Player;
 import com.tutorial.players.repositories.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +15,15 @@ import java.util.Optional;
 public class PlayerServiceDB implements PlayerService{
     @Autowired
     private PlayerRepository playerRepository;
+
+    @Override
+    @Cacheable("players")
     public List<Player> getAllPlayers() {
         return playerRepository.findAll();
     }
 
+    @Override
+    @Cacheable("players")
     public Player getPlayer(Long id) {
         Optional<Player> player = playerRepository.findById(id);
         if(player.isPresent())
@@ -24,14 +32,20 @@ public class PlayerServiceDB implements PlayerService{
             return null;
     }
 
+    @Override
+    @Cacheable("players")
     public void addPlayer(Player player) {
-        playerRepository.saveAndFlush(player);
+        playerRepository.save(player);
     }
 
+    @Override
+    @CachePut(value="players", key="#player.id")
     public void updatePlayer(Long id, Player player) {
-        playerRepository.saveAndFlush(player);
+        playerRepository.save(player);
     }
 
+    @Override
+    @CacheEvict(value="players", key="#id")
     public void deletePlayer(Long id) {
         playerRepository.deleteById(id);
     }
